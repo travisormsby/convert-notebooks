9a3c267532bee8ffd8d7ae0a04be439bea029bcd
Travis Ormsby <travis.ormsby@gmail.com>
2022-02-03 20:25:40 -0600

    add git to docker image & export ipynb to html not html_ch

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -4,7 +4,7 @@ echo $INPUT_CHANGED_FILES
 
 for FILE in $INPUT_CHANGED_FILES; do
   if [[ $FILE == *.ipynb ]]; then
-    jupyter nbconvert --template=collapsible_headings --to html_ch $FILE
+    jupyter nbconvert --template=collapsible_headings --to html $FILE
     git add $FILE
   fi
 done
