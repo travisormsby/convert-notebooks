c648d40f2326bc8e4163c3846b2ae3acc84b38bf
Travis Ormsby <travis.ormsby@gmail.com>
2022-02-04 20:02:05 -0600

    add output variable for docker action

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+OUTPUT_CHANGED_HTML=""
 for FILE in $INPUT_CHANGED_FILES; do
   if [[ $FILE == *.ipynb ]]; then
     jupyter nbconvert --template=collapsible_headings --to html_ch $FILE
+    CHANGED_HTML="${FILE%.*}.html"
+    OUTPUT_CHANGED_HTML+="${CHANGED_HTML} "
   fi
 done
+
+echo "::set-output name=CHANGED_HTML::${OUTPUT_CHANGED_HTML}"
