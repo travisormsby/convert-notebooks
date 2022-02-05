84c4ef9122c7026c9b2af406e4d5e999247dfaca
Travis Ormsby <travis.ormsby@gmail.com>
2022-02-04 18:41:24 -0600

    remote git commands from entrypoint, remove workflow steps that duplicate docker action

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-echo $INPUT_CHANGED_FILES
-
 for FILE in $INPUT_CHANGED_FILES; do
   if [[ $FILE == *.ipynb ]]; then
     jupyter nbconvert --template=collapsible_headings --to html_ch $FILE
-    git add $FILE
   fi
 done
-
-git commit -m "update html"
-git push
