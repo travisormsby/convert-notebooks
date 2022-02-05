98ae9ae397f18759f29d88d8af9933ffd8b8f443
Travis Ormsby <travis.ormsby@gmail.com>
2022-02-04 20:35:50 -0600

    test removal of extraneous output declarations

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -3,10 +3,10 @@
 OUTPUT_CHANGED_HTML=""
 for FILE in $INPUT_CHANGED_FILES; do
   if [[ $FILE == *.ipynb ]]; then
-    jupyter nbconvert --template=collapsible_headings --to html_ch $FILE
+    jupyter nbconvert --template=collapsible_headings --to html_ch --no-prompt $FILE
     CHANGED_HTML="${FILE%.*}.html"
     OUTPUT_CHANGED_HTML+="${CHANGED_HTML} "
   fi
 done
 
-echo "::set-output name=CHANGED_HTML::${OUTPUT_CHANGED_HTML}"
+# echo "::set-output name=CHANGED_HTML::${OUTPUT_CHANGED_HTML}"
